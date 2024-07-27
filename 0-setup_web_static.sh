8b118f270e5fe2bb859a04ebbc8b0e8bf9a7c181
VictorMaxima <victormaxchidera@gmail.com>
2024-07-27 02:07:19 +0100

    almost

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# to setup an nginx server 
+
+apt-get update
+apt-get install -y nginx
+
+mkdir -p /data/webstatic
+mkdir -p /data/webstatic/releases/test
+mkdir -p /data/webstatic/shared
+echo "VictorMaxima is here" > /data/webstatic/releases/test/index.html
+
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
+
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://victormaxima.pythonanywhere.com/;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
+service nginx restart
