f6c5dd459d42620e9df84104741c52b5eae735ff
VictorMaxima <victormaxchidera@gmail.com>
2024-07-28 19:59:02 +0100

    start_flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" module for a script that sets up a simple flask application """
+import flask
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def index():
+    """ view function for the home page """
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" module for a script that sets up a simple flask application """
+import flask
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def index():
+    """ view function for the home page """
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def view_hbnh():
+    """ view function for hbnb """
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
