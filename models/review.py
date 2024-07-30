08ccebd22424c9824d37e20f5b9eeabf64315a6b
VictorMaxima <victormaxchidera@gmail.com>
2024-07-30 12:10:42 +0100

    almost

--- models/review.py
+++ models/review.py
@@ -11,3 +11,5 @@ class Review(BaseModel, Base):
     place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     text = Column(String(1024), nullable=False)
+    place = relationship("Place", back_populates="reviews")
+    user = relationship("User", back_populates="reviews")
